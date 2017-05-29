8e1d4a9495fe716afe24f3671177354e9742491c
BMO <bmo@wpengine.com>
2017-05-29 00:19:00 -0500

    Starting work on Query Builder and DAO classes

--- src/Query_Builder.class.php
+++ src/Query_Builder.class.php
@@ -4,4 +4,32 @@ namespace War_Api\Data;
 
 class Query_Builder {
 
+	private $model;
+	private $params;
+
+	public function __construct( $model = array(), $params = array() ){
+		$this->model = $model;
+		$this->params = $params;
+	}
+
+	public function get_all(){
+
+	}
+
+	public function get_item(){
+
+	}
+
+	public function create_item(){
+
+	}
+
+	public function update_item(){
+
+	}
+
+	public function delete_item(){
+
+	}
+	
 }
